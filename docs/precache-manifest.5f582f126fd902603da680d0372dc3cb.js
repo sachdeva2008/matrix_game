5b0ad159f9d871ec296f4c1016b1ad8c46a621ce
Mohit <sach2008@outlook.com>
2019-04-07 11:36:15 +0530

    new production build

---
+++ docs/precache-manifest.5f582f126fd902603da680d0372dc3cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f657b0fdf6194b593ae",
+    "url": "matrix_game/static/js/runtime~main.797f21bf.js"
+  },
+  {
+    "revision": "2243c55054afa1c39188",
+    "url": "matrix_game/static/js/main.642ff5f4.chunk.js"
+  },
+  {
+    "revision": "aacd495ff772ce37c54c",
+    "url": "matrix_game/static/js/2.e7ef50d8.chunk.js"
+  },
+  {
+    "revision": "2243c55054afa1c39188",
+    "url": "matrix_game/static/css/main.abbadc6f.chunk.css"
+  },
+  {
+    "revision": "6acabceb24ae26596aa9aca710beb548",
+    "url": "matrix_game/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6fe34b310b56f71660a3622921b7cf72.js"
+  "matrix_game/precache-manifest.5f582f126fd902603da680d0372dc3cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("matrix_game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
